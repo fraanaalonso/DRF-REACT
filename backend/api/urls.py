f1a53320d9c1c61305eab966dab5cf7753b9a11e
Fran Alonso <fraloal97@gmail.com>
2021-04-24 21:34:40 +0200

    api urls

---
+++ backend/api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+app_name='api'
+
+urlpatterns = [
+    path('auth/', include('accounts.urls', namespace='accounts')),
+    path('', include('posts.urls', namespace='posts')),
+    
+    
+    #token
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+]
