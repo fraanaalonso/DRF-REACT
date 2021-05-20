36c33e1f2f05d1ccde3bef7c3771ecbc6a2462ef
Fran Alonso <fraloal97@gmail.com>
2021-05-20 18:16:10 +0200

    Frontend validation contact form

--- frontend/src/components/sections/Contact.js
+++ frontend/src/components/sections/Contact.js
@@ -1,13 +1,44 @@
 import React from 'react'
+import Swal from 'sweetalert2';
+import { useForm } from '../../hooks/useForm';
+
+import validator from 'validator';
 
 export const Contact = () => {
+
+    const {values, handleInputChange } = useForm({
+        name: '',
+        email: '',
+        subject: '',
+        content: '',
+    })
+
+    const { name, email, subject, content } = values;
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if( validator.isEmpty(name) ||
+            validator.isEmpty(email) ||
+            validator.isEmpty(subject) ||
+            validator.isEmpty(content) ){
+                Swal.fire('Error', 'You must fill all the fields', 'error');
+                return;
+            }
+        
+        if( !validator.isEmail(email) ){
+            Swal.fire('Error', 'Email not valid', 'error');
+            return;
+        }
+        
+    }
     return (
         <section id="contact">
-            <form className="contact__form">
-                <input type="text" placeholder='Name' />
-                <input type="email" placeholder='Email' />
-                <input type="text" placeholder='Subject' />
-                <textarea name="message" placeholder='Write here...'></textarea>
+            <form className="contact__form" onSubmit={ handleSubmit }>
+                <input type="text" placeholder='Name' name='name' value= {name} onChange={ handleInputChange} />
+                <input type="text" placeholder='Email' name='email' value={email}  onChange={ handleInputChange} />
+                <input type="text" placeholder='Subject' name='subject' value={ subject } onChange={ handleInputChange } />
+                <textarea placeholder='Write here...' name='content' value={ content } onChange={ handleInputChange }></textarea>
 
                 <input type="submit" value='Submit' />
             </form>
